b4a73c272c0511f3ed73d8a6e92ae50d8429cc90
dfour001 <53315984+dfour001@users.noreply.github.com>
2021-01-05 13:48:58 -0500

    Completed script

--- downloadPDFs.py
+++ downloadPDFs.py
@@ -7,7 +7,12 @@ import os
 # First I need to get the link to the page for each song
 # then download the pdf from the song page.
 
-songListURL = "https://www.guitardownunder.com/fingerstyle.php"
+URLs = {
+    "Fingerstyle":"https://www.guitardownunder.com/fingerstyle.php",
+    "Classical":"https://www.guitardownunder.com/classical.php",
+    "Ensemble":"https://www.guitardownunder.com/ensemble.php"
+}
+songListURL = ""
 baseURL = "https://www.guitardownunder.com/"
 
 def get_song_page_list(soup):
@@ -29,18 +34,18 @@ def get_pdf_url(soup):
     return pdfURL
 
 
-def download_pdf(url):
+def download_pdf(url, folderName):
     """ Downloads the pdfs from the input song page urls """
     fileName = url.split('/')[-1]
-    if not os.path.exists('sheetmusic'):
-        os.makedirs('sheetmusic')
+    if not os.path.exists(folderName):
+        os.makedirs(folderName)
 
-    if fileName not in os.listdir('sheetmusic'):
+    if fileName not in os.listdir(folderName):
         print(f'Downloading {fileName}...')
         try:
             pdf = get(url)
 
-            with open(f'sheetmusic/{fileName}', 'wb') as file:
+            with open(f'{folderName}/{fileName}', 'wb') as file:
                 file.write(pdf.content)
         except:
             print(f'Error downloading {fileName}')
@@ -60,11 +65,23 @@ def get_soup(url):
 
 
 if __name__ == "__main__":
-    soup = get_soup(songListURL) # Get song contents page as BeautifulSoup object
-    urlList = get_song_page_list(soup) # Get urls for each song in contents
-    
-    # For url in urlList, get list of PDFs to download
-    for url in urlList:
-        songSoup = get_soup(url) # Get BeautifulSoup object of song page
-        pdfURL = get_pdf_url(songSoup)
-        download_pdf(pdfURL)
+    count = 0
+    errorList = []
+    for name, url in URLs.items():
+        soup = get_soup(url) # Get song contents page as BeautifulSoup object
+        urlList = get_song_page_list(soup) # Get urls for each song in contents
+
+        # For url in urlList, get list of PDFs to download
+        for url in urlList:
+            try:
+                songSoup = get_soup(url) # Get BeautifulSoup object of song page
+                pdfURL = get_pdf_url(songSoup)
+                download_pdf(pdfURL, folderName=name)
+                count += 1
+            except Exception as e:
+                print(f'Error downloading pdf in {url}.')
+                print(str(e))
+                errorList.append(url)
+
+    print(f'{count} copied succesfully')
+    print(f'errorList: {errorList}')
